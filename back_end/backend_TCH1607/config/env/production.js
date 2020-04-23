37181578c709c55a3e695c2a9d3f638b87ede1fe
Nghia <49479525+nghiadcgch17136@users.noreply.github.com>
2020-04-23 16:33:08 +0700

    *

--- back_end/backend_TCH1607/config/env/production.js
+++ back_end/backend_TCH1607/config/env/production.js
@@ -195,10 +195,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      '*',
+    ],
 
 
     /***************************************************************************
